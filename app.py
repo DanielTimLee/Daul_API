c178ad94b4fbe28571484b0ae2e82f8cac194697
Daniel Tim Lee <danieltimlee@gmail.com>
2016-08-31 04:59:50 +0900

    간단한 로그인 API 구현 (Get 방식 이용)
    DB 이용하지 않고 ID, PW 값을 Get Parameter 로 넘겨서 일치하는지 비교해줌
    
    curl "localhost:8880/login?username=admin&password=1234"
    
    터미널에서 이렇게 명령어 보냈을 때 "Hi Admin!" 라 돌아오면 정상.

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-from flask import Flask
+from flask import Flask,request
 app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+@app.route('/login',methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    if username == 'admin' and password == '1234':
+        return "Hi Admin!!"
+    else:
+        return "Back Off Stranger!!"
+
 app.run(host='0.0.0.0',port=8880,debug=True)
