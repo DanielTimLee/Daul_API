995debff4439d967e49475fbbf79a1483061a49b
Daniel Tim Lee <danieltimlee@gmail.com>
2016-09-04 23:09:51 +0900

    Semantic UI 를 이용해서 뷰 구현. + Jinja block, url 등 처리.
    lib 설치해준 static 폴더를 app 객체에 명시
    
    Semantic UI 참고
    http://semantic-ui.com/
    
    Jinja block, url 등 사용법 참고
    http://jinja.pocoo.org/

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
-
-# 이런식으로 임포트 해도 됨 (url중 __all__ 참조)
-# from app.models import user , ...
-# https://wikidocs.net/1418#all
 
 from app.models import *
 
